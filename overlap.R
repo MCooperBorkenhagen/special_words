070a6b2dd68c73d8ce3a0bae4a03043b2e9be2ff
MCooperBorkenhagen <cooperborken@wisc.edu>
2022-08-04 23:21:28 -0500

    working out overlap section

---
+++ overlap.R
@@ -0,0 +1,7 @@
+
+nsd = Zs %>% 
+  group_by(source, var) %>% 
+  summarise(SD = sd(Z, na.rm = T),
+            M_ = mean(Z, na.rm = T),
+            n = n(),
+            source_order = first(source_order))
